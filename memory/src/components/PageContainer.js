d3acefe8e9520cf690d3b10bbd83da2a677d8728
Nic_Logan <nicholaslogan255@gmail.com>
2018-08-14 13:41:03 -0500

    Almost complete

--- memory/src/components/PageContainer.js
+++ memory/src/components/PageContainer.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import Scorebar from "./scorebar";
 import Photo from "./photo";
+import data from "../data.json";
 
 
 class PageContainer extends Component {
     state = {
+        data,
         cur_score: 0,
         high_score: 0
     };
 
     // When this component mounts, do something
     componentDidMount() {
+        this.shuffleData();
+        this.resetData();
 
     }
 
@@ -23,15 +27,134 @@ class PageContainer extends Component {
         });
     };
 
+   
+
+    // return the object from our data array with the same id
+    findObject = (id) => {
+       // console.log("Finding id: ",id);
+        const mydata = this.state.data;
+
+        //console.log("Our data:",this.state.data);
+
+        // loop through every object, look for matching ids
+        for(let i = 0; i < this.state.data.length; i++){
+            if(this.state.data[i].id === id){
+                //console.log("Found: ",this.state.data[i]);
+                return this.state.data[i]
+            }
+        }
+    }
+
+    // returns the index in the data array of the object with id: id
+    findDataIndex = (id) => {
+        // loop through every object, look for matching ids
+        for(let i = 0; i < this.state.data.length; i++){
+            if(this.state.data[i].id === id){
+                return i;
+            }
+        }
+    }
+
+
+    resetData = () => {
+        // create copy of the state
+        let stateCopy = this.state;
+
+        // set every object's clicked attr to false
+        for(let i =0; i< stateCopy.data.length; i++){
+            stateCopy.data[i].clicked = false;
+        }
+
+        // set state with the new info
+        this.setState({stateCopy});
+    };
+
+    shuffleData = () => {
+
+        let stateCopy = this.state;
+        let len = stateCopy.data.length
+
+
+        let i;
+        let j;
+
+        // Perform 2n swaps on the object array (n= number of elements to choose from) 
+        for( let x = len*2; x>0; x--){
+            // pick two random elements
+            i = Math.floor(Math.random() *len);
+            j = Math.floor(Math.random() * len);
+           
+            // swap them
+            const temp = stateCopy.data[i];
+            stateCopy.data[i] = stateCopy.data[j];
+            stateCopy.data[j] = temp;
+            
+        }
+
+        // set state with the new info
+        this.setState({stateCopy});
+    };
+
+    // function for when a photo has been clicked
+  handlePhotoClicked = (id) => {
+    //console.log( "Photo Clicked")
+
+    // create copy of the state
+    let stateCopy = this.state;
+
+    console.log("Clicked on: ",this.findObject(id));
+
+    // check if the photo has been click on 
+    if(this.findObject(id).clicked){
+        console.log("Already Clicked")
+
+        // if it has already been clicked on, reset the game
+
+        // TODO: ADD MODAL
+        alert("You already clicked that!");
+       
+        // reset score to zero
+        stateCopy.cur_score =0;
+
+        this.resetData();
+
+        // set state with the new info
+        this.setState({stateCopy});
+    }
+    else{
+        console.log("Not Clicked")
+
+        // update this objects clicked attribute
+        stateCopy.data[this.findDataIndex(id)].clicked = true;
+
+        // update score
+        stateCopy.cur_score +=1;
+
+        // update high score if needed
+        if(stateCopy.cur_score > stateCopy.high_score){
+            stateCopy.high_score = stateCopy.cur_score
+        }
+
+         // set state with the new info
+        this.setState({stateCopy});
+        
+    }
+
+    this.shuffleData();
+    
+
+    
+  };
+
     // load the page with the SearchForm and the ResultList components
     render() {
         return (
-            <div class="App">
-                <div class="App-header">
-                    <div class="App-title header-center">
+            <div className="App">
+                <div className="App-header">
+                    <div className="App-title header-center">
                         Memory Game
                     </div>
-                    <div class="App-description header-center">
+                    <div className="App-description header-center">
                     Select as many unique pictures as you can. If you click on an image you have already picked, you have
                     to start over! Try to get all of the pictures!
         
@@ -39,27 +162,28 @@ class PageContainer extends Component {
                     
                 </div>
 
+                {/* our scorebar */}
                 <Scorebar
                     curscore={this.state.cur_score}
                     highscore={this.state.high_score}
                 />
 
-                <div class="photo-container">
+    
 
-                <Photo />
-                <Photo photo_url="../assets/steven"/>
-                <Photo photo_url="../assets/steven"/>
-                <Photo photo_url="../assets/steven"/>
-                <Photo photo_url="../assets/steven"/>
-                <Photo photo_url="../assets/steven"/>
-                <Photo photo_url="../assets/steven"/>
-                <Photo photo_url="../assets/steven"/>
-                <Photo photo_url="../assets/steven"/>
-                  
+                <div className="photo-container">
+
+                {this.state.data.map(item => (
+                    <Photo
+                        key={item.id}
+                        id={item.id}
+                        // shake={!this.state.score && this.state.topScore}
+                        handleClick={this.handlePhotoClicked}
+                        image={item.image}
+                    />
+                  ))
+                }
+
                 </div>
-
-
-
 
             </div>
         );
--- memory/src/components/photo.js
+++ memory/src/components/photo.js
@@ -1,26 +1,15 @@
 import React from "react";
 
-// By extending the React.Component class, Counter inherits functionality from it
-class Photo extends React.Component {
-
-  // Setting the initial state of the Counter component
-  state = {
-    clicked: 0
-  };
-
-  // photoClicked updates the state 
-  photoClicked = () => {
-    this.state.clicked == 0 ?
-    this.setState({ clicked: 1}):
-    this.setState({ clicked: 2})
-  };
-
-  // The render method returns the JSX that should be rendered
-  render() {
-    return (
-      <img   onClick={this.photoClicked} className="photo" scr={this.photo_url}/>
-    );
-  }
-}
-
-export default Photo;
+const Photo = props => (
+    <div 
+    id={props.id} 
+    className = "photo"
+    key = {props.key}
+    style ={{ backgroundImage: `url("${props.image}")` }}
+    onClick={() => props.handleClick(props.id)}
+    >
+       
+    </div>
+  );
+  
+  export default Photo;
